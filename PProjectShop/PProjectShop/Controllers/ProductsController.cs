8e83f53e824b59f69db30b122a252c9e00c879aa
Rozzini <Rozzinimr@gmail.com>
2020-12-12 15:48:57 +0300

    Project and gitignore were added

---
+++ PProjectShop/PProjectShop/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PProjectShop.Repository.Interfaces;
+using PProjectShop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PProjectShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ProductsController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        [HttpGet]
+        public IActionResult ShowAllProducts()
+        {
+            ProductsViewModel products = new ProductsViewModel();
+            products.ProductsList = _productRepository.GetAllProducts();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult ShowProductsByCategory(int id)
+        {
+            ProductsViewModel products = new ProductsViewModel();
+            products.ProductsList = _productRepository.GetProductsByCategory(id);
+            return View(products);
+        }
+    }
+    
+}
