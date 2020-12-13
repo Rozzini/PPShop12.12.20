751f417265a28442ceb5d0569ed3b36fdc2c719d
Rozzini <Rozzinimr@gmail.com>
2020-12-13 06:47:52 +0300

    Project added

---
+++ PProjectShop/PProjectShop/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PProjectShop.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+
+        public string ClientName { get; set; }
+    }
+}
