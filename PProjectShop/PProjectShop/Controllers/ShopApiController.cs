690bfc3d62dbcda50216c30b7c8123d793fa6c47
Rozzini <Rozzinimr@gmail.com>
2020-12-14 10:34:20 +0300

    Some methods were added to api controller

--- PProjectShop/PProjectShop/Controllers/ShopApiController.cs
+++ PProjectShop/PProjectShop/Controllers/ShopApiController.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PProjectShop.Models;
+using PProjectShop.Repository;
+using PProjectShop.ViewModels;
 
 namespace PProjectShop.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[action]")]
     [ApiController]
     public class ShopApiController : ControllerBase
     {
+        private readonly GeneralDataAccessRepository _generalDataAccessRepository;
+
+        public ShopApiController(GeneralDataAccessRepository generalDataAccessRepository)
+        {
+            _generalDataAccessRepository = generalDataAccessRepository;
+        }
+        [HttpGet]
+        public IEnumerable<Product> ShowAllProducts()
+        {
+            //var products = new ProductsViewModel
+            //{
+            //    ProductsList = _generalDataAccessRepository.GetAllProducts()
+            //};
+
+            return _generalDataAccessRepository.GetAllProducts();
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> ShowProductsByCategory(Guid id)
+        {
+            //var products = new ProductsViewModel
+            //{
+            //    ProductsList = _generalDataAccessRepository.GetProductsByCategory(id)
+            //};
+            return _generalDataAccessRepository.GetProductsByCategory(id);
+        }
     }
 }
