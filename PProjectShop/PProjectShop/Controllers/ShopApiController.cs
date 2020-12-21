00e280eb52568c96ed3f6ffce0c544b7ec100c70
Rozzini <Rozzinimr@gmail.com>
2020-12-21 20:07:56 +0300

    Finally working API

--- PProjectShop/PProjectShop/Controllers/ShopApiController.cs
+++ PProjectShop/PProjectShop/Controllers/ShopApiController.cs
@@ -23,25 +23,51 @@ namespace PProjectShop.Controllers
         [HttpGet]
         public IEnumerable<Product> ShowAllProducts()
         {
-            //var category = new Category
-            //{
-            //    CategoryName = "aaa",
-            //    CategoryDescription = "aaa",
-            //};
+            var category = new Category
+            {
+                CategoryName = "aaa",
+                CategoryDescription = "aaa",
+            };
 
-            //_generalDataAccessRepository.CreateCategory(category);
+         
 
-            //Guid catId = _generalDataAccessRepository.GetCategoryId(category.CategoryName);
-            //var product = new Product
-            //{
-            //    ProductName = "IIIII",
-            //    ProductDescription = "asdasdasd",
-            //    ProductPrice = 800,
-            //    ProductImage = "~/images/IPhone1.jfif",
-            //    CategoryId = catId
-            //};
-
-            //_generalDataAccessRepository.CreateProduct(product);
+            Guid catId = _generalDataAccessRepository.GetCategoryId(category.CategoryName);
+            var product = new Product
+            {
+                ProductName = "IPhon3",
+                ProductDescription = "PlaceHolder Text1",
+                ProductPrice = 200,
+                ProductImage = "~/images/IPhone1.jfif",
+                CategoryId = catId
+            };
+            var product1 = new Product
+            {
+                ProductName = "IPhon2",
+                ProductDescription = "PlaceHolder Text2",
+                ProductPrice = 2000,
+                ProductImage = "~/images/IPhone1.jfif",
+                CategoryId = catId
+            };
+            var product2 = new Product
+            {
+                ProductName = "XIAMOI",
+                ProductDescription = "PlaceHolder Text3",
+                ProductPrice = 1500,
+                ProductImage = "~/images/IPhone1.jfif",
+                CategoryId = catId
+            };
+            var product3 = new Product
+            {
+                ProductName = "Detroit",
+                ProductDescription = "PlaceHolder Text4",
+                ProductPrice = 400,
+                ProductImage = "~/images/IPhone1.jfif",
+                CategoryId = catId
+            };
+            _generalDataAccessRepository.CreateProduct(product1);
+            _generalDataAccessRepository.CreateProduct(product2);
+            _generalDataAccessRepository.CreateProduct(product3);
+            _generalDataAccessRepository.CreateProduct(product);
 
             return _generalDataAccessRepository.GetAllProducts();
         }
--- pp-shop-react/src/types/Product.tsx
+++
@@ -1,6 +0,0 @@
-export interface Product {
-    name: string;
-    description: string;
-    image: string;
-    price: any;
-}
