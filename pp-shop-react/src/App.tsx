8507f0930f9d6063021e11e4f9baa0fbb351ecd7
Rozzini <Rozzinimr@gmail.com>
2020-12-25 14:11:32 +0300

    added material ui cards

--- pp-shop-react/src/App.tsx
+++ pp-shop-react/src/App.tsx
@@ -1,25 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import axios, { CancelTokenSource } from "axios";
-
 import {IProduct} from "./models"
 import {ProductsList} from "./components/ProductsList";
+import React from 'react';
 import {fetchAllProducts} from './services/api';
 
 const defaultProps:IProduct[] = [];
 
 function App() {
 const [products, setProducts] = React.useState<IProduct[]>(defaultProps) 
-const [loading, setLoading]: [boolean, (loading: boolean) => void] = React.useState<boolean>(true);
-const [error, setError]: [string, (error: string) => void] = React.useState("");
-
 React.useEffect(() => { 
   fetchAllProducts(setProducts);
 }, [])
 
 return (
-<div>
-  <ProductsList products={products}/>
-</div>
+      <div>
+        <ProductsList products={products}/>
+      </div>
 );
 }
 export default App;
